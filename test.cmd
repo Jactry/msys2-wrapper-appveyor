f5f11b364c4835767bff677b0d443b8414622b7b
Jactry Zeng <jzeng@codeweavers.com>
2015-08-08 17:21:27 +0800

    Update.

--- test.cmd
+++ test.cmd
@@ -1,6 +1,7 @@
 set MSYS2_PACKAGE=msys2-base-i686-20150512-20150808.7z
 
-7z x %MSYS2_PACKAGE% -oC:\ProgramData\ > nul
+7z x %MSYS2_PACKAGE% -oC:\ProgramData\
 
 SET PATH=C:\ProgramData\msys32\wrapper\;%PATH%
+echo "Test MSYS2"
 msys2run.cmd "echo Hello"
