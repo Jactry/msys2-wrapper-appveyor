c7f34b7a3ae6dc4faf87b8991adaaa88d2555810
Jactry Zeng <jzeng@codeweavers.com>
2015-08-08 21:05:50 +0800

    Add time and date.

--- test.cmd
+++ test.cmd
@@ -1,7 +1,7 @@
 set MSYS2_PACKAGE=msys2-base-i686-20150512-20150808.7z
-
-7z x %MSYS2_PACKAGE% -oC:\ProgramData\
-
+time /t
+7z x %MSYS2_PACKAGE% -oC:\ProgramData\ > nul
+time /t
 SET PATH=C:\ProgramData\msys32\wrapper\;%PATH%
 echo "Test MSYS2"
-msys2run.cmd "echo Hello"
+msys2run.cmd "pacman -S --needed --noconfirm zsh"
